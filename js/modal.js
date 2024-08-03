602887b80ac6730ac4bf33ee41e6aa2fa8cd7388
eziquexx <waftyann@gmail.com>
2024-08-04 01:30:47 +0900

    modal 작업 fin

---
+++ js/modal.js
@@ -0,0 +1,21 @@
+const modal = document.querySelector('#galleryModal');
+const galleryImgGroup = document.querySelectorAll('.studioImgGroup > li');
+const modalImg = document.querySelector('#galleryModal > .modal-content');
+const closeBtn = document.querySelector('#galleryModal .close');
+
+galleryImgGroup.forEach(img => img.addEventListener("click", () => {
+  modal.style.display = "block";
+  modalImg.src = img.children[0].src;
+
+  modal.onmouseover = () => {
+    document.body.style.overflowY = "hidden";
+  }
+  modal.onmouseout = () => {
+    document.body.style.overflowY = "auto";
+  }
+}));
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+

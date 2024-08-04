9c76ba074f089bb8b5224917e2a9d7a2a53488f0
eziquexx <waftyann@gmail.com>
2024-08-04 09:34:59 +0900

    modal mouse event test-6

--- js/modal.js
+++ js/modal.js
@@ -3,7 +3,7 @@ const galleryImgGroup = document.querySelectorAll('.studioImgGroup > li');
 const modalImg = document.querySelector('#galleryModal > .modal-content');
 const closeBtn = document.querySelector('#galleryModal .close');
 
-galleryImgGroup.forEach(img => img.addEventListener("click", () => {
+galleryImgGroup.forEach(img => img.addEventListener("click", (e) => {
   modal.style.display = "block";
   modalImg.src = img.children[0].src;
 
@@ -11,14 +11,14 @@ galleryImgGroup.forEach(img => img.addEventListener("click", () => {
     modal.style.display = "none";
 
     if (modal.style.display = 'none') {
-      document.body.style.overflowY = 'auto';
-      document.removeEventListener('touchmove', preventDefault, false);
+      document.body.style.overflow = 'auto';
+      document.removeEventListener('touchmove', e.preventDefault, false);
     }
   });
 
   if(modal.style.display = 'block') {
-    document.body.style.overflowY = 'hidden';
-    document.addEventListener('touchmove', preventDefault, false);
+    document.body.style.overflow = 'hidden';
+    document.addEventListener('touchmove', e.preventDefault, false);
   } 
   
 }));
